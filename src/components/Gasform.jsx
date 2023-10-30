ded2315a1b9aa2b43b20fe7c56515a7faaca3110
Olayemi <ogunrinde_olayemi@yahoo.com>
2023-10-30 03:20:37 -0700

    Updated form component

--- src/components/Gasform.jsx
+++ src/components/Gasform.jsx
@@ -71,32 +71,35 @@ const GasForm = () => {
       <div className="w-full md:w-1/2 h-1/2 md:h-screen flex justify-center items-center bg-gray-800">
         <div className="w-4/5 md:w-3/5 lg:w-2/2 p-8">
           <h1 className="text-xl font-bold mb-4"> Gas Refill Notification Alert</h1>
-          <select className='text-black py-2 mb-2 rounded w-full' value={gasSize} onChange={handleGasSizeChange }>
-            <option value="1kg">1kg</option>
-            <option value="5kg">5kg</option>
-            <option value="16kg">16kg</option>
-            <option value="12.5kg">12.5kg</option>
-          </select>
-          <input
-            className="text-black w-full p-2 mb-4 rounded"
-            type="date"
-            value={dateRefilled}
+          <form onSubmit={handleSubmit}>
+            <select className='text-black py-2 mb-2 rounded w-full' value={gasSize} onChange={handleGasSizeChange }>
+              <option value="1kg">1kg</option>
+              <option value="5kg">5kg</option>
+              <option value="16kg">16kg</option>
+              <option value="12.5kg">12.5kg</option>
+            </select>
+            <input
+              className="text-black w-full p-2 mb-4 rounded"
+              type="date"
+              value={dateRefilled}
             onChange={handleDateChange}
-          />
-          <input 
-          className='text-black w-full p-2 mb-4 rounded'
-          type="email"
-          placeholder='Email' 
-          value={userEmail} 
-          onChange={handleEmailChange} />
-          <div className='text-center justify-center'>
-          <button
-            className="rounded-lg px-2 py-2 bg-white text-black w-full md:w-40"
-            onClick={handleSubmit}
-          >
-            Submit
-          </button>
-          </div>
+            />
+            <input 
+              className='text-black w-full p-2 mb-4 rounded'
+              type="email"
+              placeholder='Email' 
+              value={userEmail} 
+              onChange={handleEmailChange}
+            />
+           <div className='text-center justify-center'>
+            <button
+              className="rounded-lg px-2 py-2 bg-white text-black w-full md:w-40"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+           </div>
+         </form>
         </div>
       </div>
       <div className="w-full md:w-3/3 h-1/2 md:h-screen flex justify-center items-center">
